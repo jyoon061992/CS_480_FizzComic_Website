786fa798a0f13765cbc7a64a4c8ea82d7e6f161b
gonzalezroberto <robertogonzalez0513@gmail.com>
2018-05-06 22:20:22 -0700

    start over

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
-// var express = require('express');
-// var router = express.Router();
-//
-// router.get('/message', function(req, res, next) {
-//   res.json('Welcome To React...');
-// });
-//
-// module.exports = router;
+var express = require('express');
+var router = express.Router();
+
+router.get('/message', function(req, res, next) {
+  res.json('Welcome To React');
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,78 +1,90 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-const app = express();
+#!/usr/bin/env node
 
-var mysql = require('mysql');
-var con = mysql.createConnection({
-   host: 'us-cdbr-iron-east-04.cleardb.net',
-   user: 'bb353640536722',
-   password: 'f408fb6c',
-   database: 'heroku_f8d562e61e70440',
-   pool: { maxConnections: 50, maxIdleTime: 300}
-});
+/**
+ * Module dependencies.
+ */
 
-con.connect(function(error){
-   if(!!error){
-       console.log('Error');
-   }else{
-       console.log('Connected');
-   }
-});
+var app = require('./app');
+var debug = require('debug')('express-react:server');
+var http = require('http');
 
-app.get('/api/logins', (req, res) => {
-    con.query("select * from comics", function(err, rows, fields){
-      console.log(rows.length);
-      return res.json(rows)
-      con.end();
-    })
-});
+/**
+ * Get port from environment and store in Express.
+ */
 
-app.listen(3001, function(){
-  console.log('Server started on Port 3001');
-});
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('heroku_f8d562e61e70440', 'bb353640536722', 'f408fb6c', {
-//   host: 'us-cdbr-iron-east-04.cleardb.net',
-//   dialect: 'mysql',
-//   operatorsAliases: false,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   },
-// });
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection to clearDB Database established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-// const User = sequelize.define('comics', {
-//   cid: { type: Sequelize.STRING, primaryKey: true },
-//   series: Sequelize.STRING,
-//   title: Sequelize.STRING,
-//   volume: Sequelize.STRING,
-//   coverArt: Sequelize.STRING,
-//   writers: Sequelize.STRING
-// },{
-//   timestamps: false
-// });
-// //query the database
-// sequelize.sync().then(function (){
-//   User.findById(2).then(function(users){
-//      const data1 = users.dataValues.firstname;
-//   });
-// });
-// const schema = require('./schema.js');
-//
-// app.use('/graphql', expressGraphQL({
-//   schema:schema,
-//   graphiql: true, 
-// }));
+var port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
 
-// app.listen(3001, () => {
-//   console.log('server running on port 4000..');
-// });
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
