b556261b98ccb9cbc5a6136d4edc236a380931b4
gonzalezroberto <robertogonzalez0513@gmail.com>
2018-05-06 19:11:38 -0700

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './Components/Login';
 
 class App extends Component {
   constructor() {
     super();
     this.state = { message: '' };
   }
-
-  componentDidMount() {
-    fetch('/api/message')
-      .then(response => response.json())
-      .then(json => this.setState({ message: json }));
-  }
-
+  // componentDidMount() {
+  //   fetch('/api/message')
+  //     .then(response => response.json())
+  //     .then(json => this.setState({ message: json }));
+  // }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{this.state.message}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Login/>
       </div>
     );
   }
---
+++ src/Components/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Login.css';
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { accounts: [] };
+  }
+  componentDidMount(){
+    fetch('/api/logins')
+      .then(res => res.json())
+      .then(accounts => this.setState({accounts:accounts}, () => console.log('accounts fetched', accounts)));
+   }
+  signUp()
+  {
+    console.log('this.state',this.state);
+  }
+  render() {
+    return (
+      <div className="login-form">
+        <h3>Sign Up</h3>
+        <input className ="form"
+        type ="text"
+        placeholder ="email"
+        onChange = {event => this.setState({email: event.target.value})}
+        />
+
+        <p>
+        <input className ="form"
+        type ="password"
+        placeholder ="password"
+        onChange = {event => this.setState({password: event.target.value})}
+        />
+        </p>
+        <p>
+        <button className ="submit-button"
+        type ="button"
+        onClick = {() => this.signUp()}
+        >
+        submit
+        </button>
+        </p>
+        <h5>Current comicbook database</h5>
+        <ul>
+          {this.state.accounts.map(accounts =>
+          <li key = {accounts.firstname}> <p><img src={accounts.coverArt}/></p><p>Title: {accounts.title}</p>Writers : {accounts.writers}</li>
+        )}
+        </ul>
+      </div>
+    );
+  }
+}
+export default Login;
