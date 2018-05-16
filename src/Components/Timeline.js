04f762e44d47179c1b61458b2e771378ccce4dac
pooya134 <38294574+pooya134@users.noreply.github.com>
2018-05-16 08:31:15 -0700

    update timeline

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {withRouter} from 'react-router';
 import axios from 'axios';
 import "../stylesheets/Timeline.css"
 
-class TimelineInput extends React.Component {
-  state = {
+class Timeline extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {accounts: []};
+}
 
+componentDidMount(){
+  fetch('/api/getuser')
+      .then(res => res.json())
+      .then(json =>{this.setState({accounts:json});
+    });
+}
 
-  }
+makePost(){
+  this.props.router.push('/makepost');
+}
+
+makeComment(){
+
+  
+}
 
 render(){
   return(
@@ -18,9 +35,16 @@ render(){
             <div className = "profileForm">
               <div clssName = "proContainer">
               <h1 className = "proTitle" > My Profile </h1>
-              <p><i className = "proName"></i>here goes name</p>
-              <p><i className = "proCity"></i>here goes location</p>
-              <p><i className = "proBirth"></i>Here goes birth date</p>
+
+              <p>
+              {this.state.accounts.map(user =>
+              <p>
+                <img src={user.profilepicture} width = "130" height = "130"/>
+                <p>name: {user.firstname + " " + user.lastname}</p>
+              <p>email: {user.username}</p>
+              </p>
+              )}
+              </p>
               </div>
             </div>
 
@@ -28,43 +52,42 @@ render(){
               <div className = "inputForm">
               <input className ="statInput"
               type ="text"
-              placeholder ="Status:"
+              placeholder ="Status"
+              name = "content"
               />
-              <button type = "button" className = "statButton">Post</button>
+              <button type = "button" className = "statButton"
+              onClick = {() => this.makePost()}>
+              Post</button>
               </div>
 
               <div className = "statNews">
-               <section className="statUser">
-                <h2>Here goes name</h2>
-                <p>
-                Here goes the text, Here goes the text, Here goes the text,
-                Here goes the text, Here goes the text, Here goes the text,
-                Here goes the text, Here goes the text, Here goes the text,
-                </p>
-                <button type = "button" className = "likeButton">Like</button>
-                <button type = "button" className = "commButton">Comment</button>
-                </section>
-              </div>
-              <div className = "statNews">
-               <section className="statUser">
-                <h2>Here goes name</h2>
-                <p>
-                Here goes the text, Here goes the text, Here goes the text,
-                Here goes the text, Here goes the text, Here goes the text,
-                Here goes the text, Here goes the text, Here goes the text,
-                </p>
-                <button type = "button" className = "likeButton">Like</button>
-                <button type = "button" className = "commButton">Comment</button>
-                </section>
-              </div>
-
+                <section className="statUser">
+                  <h2>Bryan Rivera</h2>
+                    <p>
+                    I went to hiking
+                    </p>
+                    <button type = "button" className = "likeButton">Like</button>
+                    <button type = "button" className = "commButton">Comment</button>
+                    </section>
             </div>
 
+            <div className = "statNews">
+              <section className="statUser">
+                <h2>Bryan Rivera</h2>
+                  <p>
+                  Roberto, Jong, Adrian, and Jordan
+                  </p>
+                  <button type = "button" className = "likeButton">Like</button>
+                  <button type = "button" className = "commButton"
+                  onClick = {() => this.makeComment()} >Comment</button>
+                  </section>
+          </div>
+          </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 }
-export default TimelineInput;
+export default Timeline;
